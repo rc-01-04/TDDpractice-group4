5d7ec4a33eaee323f7acec3c2dadbbb7b54646f5
zzz845zz <zzz845zz@gmafds>
2019-04-22 20:00:04 +0900

    first commit, make frame

---
+++ src/main/java/domain/Member.java
@@ -0,0 +1,11 @@
+package domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private String name;
+    private String gender;
+    private String cellphoneNumber;
+    private int age;
+}
