6a8ebb7cd13d51c5f3c9e6ebaf153c3813d7ef99
hyunsong <kny4950@naver.com>
2019-04-22 22:09:36 +0900

    remove testAddGender parameter

--- src/main/java/domain/Member.java
+++ src/main/java/domain/Member.java
@@ -3,11 +3,14 @@ package domain;
 import lombok.Builder;
 import lombok.Data;
 
-@Builder
 @Data
 public class Member {
     private String name;
     private String gender;
     private String cellphoneNumber;
     private int age;
+
+    public Member(){
+
+    }
 }
