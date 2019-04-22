d602d7fc3c7f44503f8df94677f1008d6510db2c
hyunsong <kny4950@naver.com>
2019-04-22 21:43:36 +0900

    create method which adds member's Gender

--- src/test/java/domain/MemberTest.java
+++ src/test/java/domain/MemberTest.java
@@ -2,7 +2,19 @@ package domain;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class MemberTest {
+
+    @Test
+    public void TestAddGender(String gender){
+            Member member=mock(Member.class);
+            member.setGender(gender);
+            verify(member).setGender(gender);
+            assertThat(member.getGender(),is(gender));
+    }
+
 }
